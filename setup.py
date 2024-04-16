ab373996f5df78974c890ae132682a356bf9f355
Fourest <sebastien.fourest@cnes.fr>
2024-04-16 18:06:55 +0000

    ajout de lngeo.geomean()

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='lenapy',
-version='0.7.2',
+version='0.7.3',
 description='First release of new lenapy architecture',
 url='#',
 author='Sebastien Fourest & EMC2 team',
