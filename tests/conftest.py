f96dc6110dbba622d9669b9330402be50d265bdc
Arthur VINCENT <arthur.vincent@csgroup.eu>
2025-04-02 09:57:39 +0200

    test: add lngeo tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from pathlib import Path
 
 import pytest
+import xarray as xr
+
+import lenapy
 
 
 @dataclass
@@ -15,3 +18,17 @@ class LenapyTestsPath:
 @pytest.fixture
 def lenapy_paths(request) -> LenapyTestsPath:
     return LenapyTestsPath(Path(__file__).parent.parent)
+
+
+@pytest.fixture(scope="session")
+def air_temperature_data():
+    """Fixture session to load et rename data from xarray tutorials."""
+    dataset = xr.tutorial.open_dataset("air_temperature")
+    return lenapy.utils.geo.rename_data(dataset)
+
+
+@pytest.fixture(scope="session")
+def ersstv5_data():
+    """Fixture session to load et rename data from xarray tutorials."""
+    dataset = xr.tutorial.open_dataset("ersstv5")
+    return lenapy.utils.geo.rename_data(dataset)
