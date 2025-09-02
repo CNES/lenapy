2b1834b53f0c18aa3a6b3c9e7afda0431b802beb
Arthur VINCENT <arthur.vincent@csgroup.eu>
2025-09-02 15:25:19 +0200

    test: wip, add climato() unit test with chunks

--- tests/utils/test_climato.py
+++ tests/utils/test_climato.py
@@ -15,3 +15,8 @@ def test_climato_coeffs(lenapy_paths):
     assert (
         sorted(clim.coeff_names) == ref_coeffs
     ), "Coeffs_climato outputs do not correspond to the ref."
+
+
+def test_climato(lenapy_paths):
+    ohc_data = xr.open_dataset(lenapy_paths.data / "ohc.nc", chunks=True)
+    clim = ohc_data.gohc.lntime.climato()
