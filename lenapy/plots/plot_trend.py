e15625c920452e62d5cadf22c1131ba832b0ef2c
miniufo <miniufo@163.com>
2025-11-13 10:32:55 +0800

    sort imports in plot_trend.py

--- lenapy/plots/plot_trend.py
+++ lenapy/plots/plot_trend.py
@@ -1,7 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import xarray as xr
-import matplotlib.pyplot as plt
 
 
 def plot_trend(data, sigma, out="OLS", freq="1M", save=None, **kwargs):
