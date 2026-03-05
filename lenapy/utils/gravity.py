496e52b4475899b01bfba3462c9f5f8f276f2b9f
hulecom <hugo.lecomte1@gmail.com>
2026-03-05 14:51:06 +0200

    Revert these changes

--- lenapy/utils/gravity.py
+++ lenapy/utils/gravity.py
@@ -398,151 +398,37 @@ def estimate_normal_gravity(
         latitude, ellipsoidal_earth=False, f_earth=f_earth
     )
 
-    geocentric_colat = latitude_to_geocentric_colatitude(
-        latitude, ellipsoidal_earth=True, f_earth=f_earth
+    e_prime = np.sqrt(2 * f_earth - f_earth**2) / (1 - f_earth)
+    q0 = (0.5 + 1.5 / e_prime**2) * np.arctan(e_prime) - 1.5 / e_prime
+    q0_prime = 3 * (1 + 1 / e_prime**2) * (1 - 1 / e_prime * np.arctan(e_prime)) - 1
+    m = omega_earth**2 * a_earth**3 * (1 - f_earth) / earth_gravity_constant
+
+    gamma_e = (
+        earth_gravity_constant
+        / (a_earth**2 * (1 - f_earth))
+        * (1 - m - m * e_prime * q0_prime / (6 * q0))
+    )
+    gamma_p = (
+        earth_gravity_constant / a_earth**2 * (1 + m * e_prime * q0_prime / (3 * q0))
     )
 
-    if "somigliana" in mode:
-        e_prime = np.sqrt(2 * f_earth - f_earth**2) / (1 - f_earth)
-        q0 = (0.5 + 1.5 / e_prime**2) * np.arctan(e_prime) - 1.5 / e_prime
-        q0_prime = 3 * (1 + 1 / e_prime**2) * (1 - 1 / e_prime * np.arctan(e_prime)) - 1
-        m = omega_earth**2 * a_earth**3 * (1 - f_earth) / earth_gravity_constant
+    gamma_0 = (
+        gamma_e * np.sin(geographic_colat) ** 2
+        + (1 - f_earth) * gamma_p * np.cos(geographic_colat) ** 2
+    ) / np.sqrt(
+        np.sin(geographic_colat) ** 2
+        + (1 - f_earth) ** 2 * np.cos(geographic_colat) ** 2
+    )
 
-        gamma_e = (
-            earth_gravity_constant
-            / (a_earth**2 * (1 - f_earth))
-            * (1 - m - m * e_prime * q0_prime / (6 * q0))
-        )
-        gamma_p = (
-            earth_gravity_constant
-            / a_earth**2
-            * (1 + m * e_prime * q0_prime / (3 * q0))
-        )
-
-        if "reduced" in mode:
-            gamma_0 = (
-                gamma_e
-                * (
-                    1
-                    + ((1 - f_earth) * gamma_p / gamma_e - 1)
-                    * np.cos(geographic_colat) ** 2
-                )
-                / np.sqrt(
-                    1 - (2 * f_earth - f_earth**2) * np.cos(geographic_colat) ** 2
-                )
-            )
-
-        elif "_serie" in mode:
-            beta = 5.3024e-3
-            beta1 = -5.8e-6
-            gamma_0 = gamma_e * (
-                1
-                + beta * np.cos(geographic_colat) ** 2
-                + beta1 * np.cos(2 * geographic_colat) ** 2
-            )
-
-        elif "_GRS80serie" in mode:
-            c1 = 5.2790414e-3
-            c2 = 2.32718e-5
-            c3 = 1.262e-7
-            c4 = 7e-10
-            gamma_0 = gamma_e * (
-                1
-                + c1 * np.cos(geographic_colat) ** 2
-                + c2 * np.cos(geographic_colat) ** 4
-                + c3 * np.cos(geographic_colat) ** 6
-                + c4 * np.cos(geographic_colat) ** 8
-            )
-        else:
-            gamma_0 = (
-                gamma_e * np.sin(geographic_colat) ** 2
-                + (1 - f_earth) * gamma_p * np.cos(geographic_colat) ** 2
-            ) / np.sqrt(
-                np.sin(geographic_colat) ** 2
-                + (1 - f_earth) ** 2 * np.cos(geographic_colat) ** 2
-            )
-
-        gamma = gamma_0 * (
-            1
-            - 2
-            * (1 + f_earth + m - 2 * f_earth * np.cos(geographic_colat) ** 2)
-            * height
-            / a_earth
-            + 3 * height**2 / a_earth**2
-            - 4 * height**3 / a_earth**3
-        )
-
-    elif "gravi_disturbance" in mode:
-        if radius is None:
-            radius = height + earth_radius(latitude, True, a_earth, f_earth)
-
-        if lmax is None:
-            lmax = 20
-        data_null = xr.Dataset(
-            {
-                "clm": (("l", "m"), np.zeros((lmax + 1, 1))),
-                "slm": (("l", "m"), np.zeros((lmax + 1, 1))),
-            },
-            coords={"l": np.arange(lmax + 1), "m": np.array([0])},
-        )
-        normal_field = normal_zonal_correction(
-            data_null,
-            reverse=True,
-            a_earth=a_earth,
-            earth_gravity_constant=earth_gravity_constant,
-            f_earth=f_earth,
-            omega_earth=omega_earth,
-        )
-
-        dplm = compute_plm(
-            lmax + 1,
-            np.cos(geocentric_colat),
-            mmax=0,
-            latitude=latitude,
-            derivative=True,
-        )
-
-        normal_dradius = -(
-            normal_field.lnharmo.to_grid(
-                unit="gravity",
-                latitude=latitude,
-                radius=radius,
-                ellipsoidal_earth=True,
-                a_earth=a_earth,
-                gm_earth=earth_gravity_constant,
-                f_earth=f_earth,
-            )
-        )
-
-        ds_centrifugal = centrifugal_potential_partial_derivate_radius(
-            radius=radius,
-            ellipsoidal_earth=True,
-            a_earth=a_earth,
-            f_earth=f_earth,
-            omega_earth=omega_earth,
-        )
-
-        if "rad" in mode:
-            return np.abs(normal_dradius + ds_centrifugal.dV_dradius).isel(longitude=0)
-
-        normal_dlatitude = normal_field.lnharmo.to_grid(
-            unit="potential",
-            latitude=latitude,
-            radius=radius,
-            ellipsoidal_earth=True,
-            plm=dplm,
-            a_earth=a_earth,
-            gm_earth=earth_gravity_constant,
-            f_earth=f_earth,
-        )
-
-        gamma = np.sqrt(
-            (normal_dradius + ds_centrifugal.dV_dradius) ** 2
-            + (normal_dlatitude + ds_centrifugal.dV_dlatitude) ** 2 / radius**2
-        ).isel(longitude=0)
-
-    else:
-        raise ValueError("mode should be either 'somigliana', 'gravi_disturbance'")
+    gamma = gamma_0 * (
+        1
+        - 2
+        * (1 + f_earth + m - 2 * f_earth * np.cos(geographic_colat) ** 2)
+        * height
+        / a_earth
+        + 3 * height**2 / a_earth**2
+        - 4 * height**3 / a_earth**3
+    )
 
     return gamma
 
